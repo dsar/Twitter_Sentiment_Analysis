5750251143c4d840d84743182d3667d7298a4fe4
Dimitrios Sarigiannis <saridimi@gmail.com>
2016-11-19 00:44:47 +0100

    update

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,14 +1,15 @@
 import nltk
+import re
 
 def filter_user(tweets):
 	"""tweets: Series"""
-	return tweets.str.replace('<user>', '@user', case=False)
+	return tweets.str.replace('<user>', '', case=False)
 
 def filter_url(tweets):
-	return tweets.str.replace('<url>', '@url', case=False)
+	return tweets.str.replace('<url>', '', case=False)
 
 def filter_hashtag(tweets):
-	return tweets.str.replace('^#', '#hashtag', case=False)
+	return tweets.str.replace('^#', '', case=False)
 
 def tokenization(tweet):
     return list(tweet.split())
@@ -16,3 +17,6 @@ def tokenization(tweet):
 def pos_tag(tweet):
     tweet = nltk.word_tokenize(tweet.lower())
     return nltk.pos_tag(tweet)
+
+def filter_repeated_chars_on_tweet(tweet):
+	return re.sub(r'(.)\1+', r'\1\1', tweet)
