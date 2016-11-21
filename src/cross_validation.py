f68743077243e996ec26ea17687e201fbe938f5c
Dimitrios Sarigiannis <saridimi@gmail.com>
2016-11-21 03:19:11 +0100

    [ADD] Cross Validation func

---
+++ src/cross_validation.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import KFold as cross_validation_KFold
+
+def cross_validation(clf , X_size, tfidf_train_vectors, classes, n_folds=10, random_state=4):
+    cv = cross_validation_KFold(X_size, shuffle = True, n_folds=n_folds, random_state=4)
+    avg_test_accuracy = np.mean(cross_val_score(clf, tfidf_train_vectors, classes, cv=cv, scoring='accuracy'))
+    return avg_test_accuracy
